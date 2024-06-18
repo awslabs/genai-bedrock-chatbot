a926bdd6805d59412d132a967522952d9919c71e
Jundong Qiao <jdqiao@amazon.com>
2024-06-18 08:27:24 -0400

    use both chatmodel and text completion model

--- code/lambda-container/connections.py
+++ code/lambda-container/connections.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+from langchain_community.chat_models import BedrockChat
 from langchain_community.llms import Bedrock
 
 
@@ -10,52 +11,67 @@ class Connections:
     kendra_rawdata_index_id = os.environ['KENDRA_INDEX_ID']
     sagemaker_pricing_database = os.environ['SAGEMAKER_PRICING_DATABASE']
     log_level = os.environ['LOG_LEVEL']
-
     kendra_client = boto3.client("kendra", region_name=region_name)
     s3_resource = boto3.resource("s3", region_name=region_name)
     bedrock_client = boto3.client("bedrock-runtime", region_name=region_name)
 
     @staticmethod
-    def get_bedrock_llm(model_name="Claude2.1", max_tokens=256, cache=False):
+    def get_bedrock_llm(model_name="Claude2.1", max_tokens=256, cache=False, mode='chat'):
         MODELID_MAPPING = {
-            "Titan": "amazon.titan-tg1-large",
             "Claude2.1": "anthropic.claude-v2:1",
             "Claude2": "anthropic.claude-v2",
             "ClaudeInstant": "anthropic.claude-instant-v1",
+            "Claude3Sonnet": "anthropic.claude-3-sonnet-20240229-v1:0",
+            "Claude3Haiku": "anthropic.claude-3-haiku-20240307-v1:0",
         }
 
         MODEL_KWARGS_MAPPING = {
-            "Titan": {
-                "maxTokenCount": max_tokens,
-                "temperature": 0,
-                "topP": 1,
-            },
             "Claude2.1": {
-                "max_tokens_to_sample": max_tokens,
                 "temperature": 0,
                 "top_p": 1,
                 "top_k": 50,
+                "stop_sequences": ["\n\nHuman"]
             },
             "Claude2": {
-                "max_tokens_to_sample": max_tokens,
+                "temperature": 0,
+                "top_p": 1,
+                "top_k": 50,
+                "stop_sequences": ["\n\nHuman"]
+            },
+            "ClaudeInstant": {
                 "temperature": 0,
                 "top_p": 1,
                 "top_k": 50,
             },
-            "ClaudeInstant": {
-                "max_tokens_to_sample": max_tokens,
+            "Claude3Sonnet": {
+                "max_tokens": max_tokens,
                 "temperature": 0,
                 "top_p": 1,
                 "top_k": 50,
+                "stop_sequences": ["\n\nHuman"],
+            },
+            "Claude3Haiku": {
+                "max_tokens": max_tokens,
+                "temperature": 0,
+                "top_p": 1,
+                "top_k": 50,
+                "stop_sequences": ["\n\nHuman"],
             },
         }
 
         model = model_name
-        llm = Bedrock(
+        llm_class = BedrockChat if mode == 'chat' else Bedrock
+
+        model_kwargs = MODEL_KWARGS_MAPPING[model]
+        if mode == 'chat':
+            model_kwargs['max_tokens'] = max_tokens
+        else:
+            model_kwargs['max_tokens_to_sample'] = max_tokens
+
+        llm = llm_class(
             client=Connections.bedrock_client,
             model_id=MODELID_MAPPING[model],
-            model_kwargs=MODEL_KWARGS_MAPPING[model],
+            model_kwargs=model_kwargs,
             cache=cache
-            # model_kwargs=json.loads(body)
         )
         return llm
